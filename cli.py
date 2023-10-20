5112d529fa9ef419bcbc6d97094a5c2f59e7f282
ShinChven <shinchven@gmail.com>
2023-10-20 18:16:42 +0800

    feat: Update config command to allow specifying max tokens and target language
    - Allow specifying max tokens and target language in the `config` command
    - Save the updated configuration in the config file with formatting
    - Output the location of the config file after saving the configuration

--- cli.py
+++ cli.py
@@ -4,27 +4,58 @@ import os
 import click
 import git
 import openai
-import requests
 
 
 @click.group()
 def cli():
     pass
 
-@cli.command()
-@click.option('--api-key', prompt='Your API key', help='The API key to use with OpenAI.')
-@click.option('--host', prompt='OpenAI Host', help='The alternative OpenAI host.')
-def config(api_key, host):
+@cli.command(help=f"Configure the CLI. Configuration will be saved to {os.path.expanduser('~/.config/git-gpt/config.json')}")
+@click.option('--api-key', help='The API key to use with OpenAI.')
+@click.option('--host', help='The alternative OpenAI host.')
+@click.option('--max-tokens', type=int, help='Maximum number of tokens for the generated message.')
+@click.option('--lang', help='Target language for the generated message.')
+def config(api_key, host, max_tokens, lang):
     config_path = os.path.expanduser('~/.config/git-gpt/config.json')
+    
+    # Load existing configuration if it exists
+    if os.path.exists(config_path):
+        with open(config_path, 'r') as config_file:
+            config_data = json.load(config_file)
+    else:
+        config_data = {}
+
+    # Update configuration with provided arguments
+    if api_key:
+        config_data['api_key'] = api_key
+    if host:
+        config_data['openai_host'] = host
+    if max_tokens:
+        config_data['max_tokens'] = max_tokens
+    if lang:
+        config_data['lang'] = lang
+
+    # Save updated configuration with formatting
     with open(config_path, 'w') as config_file:
-        json.dump({'api_key': api_key, 'openai_host': host}, config_file)
+        json.dump(config_data, config_file, indent=4, sort_keys=True)
+    
+    # Output message indicating the location of the config file
+    click.echo(f'Configuration saved to: {config_path}')
+
+
 
 
 @cli.command()
-def commit():
+@click.option('--max-tokens', default=None, help='Maximum number of tokens for the generated message.')
+@click.option('--lang', default=None, help='Target language for the generated message.')
+def commit(max_tokens, lang):
     config_path = os.path.expanduser('~/.config/git-gpt/config.json')
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
+
+    # If arguments are not provided via command line, try to get them from the config file
+    max_tokens = max_tokens or config.get('max_tokens', 100)
+    lang = lang or config.get('lang', 'en')
 
     repo = git.Repo(os.getcwd())
     diffs = repo.git.diff('--staged')  # Get textual representation of staged diffs
@@ -35,10 +66,10 @@ def commit():
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Generate a detailed commit message for the following diffs.\n You should conclude a short message first, then explain the features in a list. here's the diffs:\n{diffs}"}
+            {"role": "system", "content": f"You are a senior programmer."},
+            {"role": "user", "content": f"Generate a commit message for the following diffs with a message under 50 characters and Description under 72 characters, written in {lang}.\nThe message should start with `feat:` or `fix`. Please summarize the Description in a list.\n\ndiffs:\n{diffs}"}
         ],
-        max_tokens=100,
+        max_tokens=max_tokens,  # Using the max_tokens argument here
         stop=None
     )
 
