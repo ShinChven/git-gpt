0fbbe8b2e970a5c9b574889eae50188ccde14139
ShinChven <shinchven@gmail.com>
2023-10-20 19:30:35 +0800

    feat: Update package name
    - Update the package name from 'git-gpt' to 'gitgpt' in `setup.py`.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='git-gpt',
+    name='gitgpt',
     version='0.1.0',
     author='ShinChven',
     author_email='shinchven@gmail.com',
